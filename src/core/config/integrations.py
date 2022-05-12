cfa3cf6fa19673e294b6d9b8e4d45792004a9883
v.bogdanenok <v.bogdanenok@benovate.ru>
2022-05-12 12:05:25 +0300

    minor fixes for front-end needs

--- src/core/config/integrations.py
+++ src/core/config/integrations.py
@@ -4,11 +4,11 @@ from fastapi_mail import ConnectionConfig
 
 
 yandex_smtp_config = ConnectionConfig(
-    MAIL_USERNAME = os.environ.get('YANDEX_SMTP_USERNAME'),
+    MAIL_USERNAME = os.environ.get('YANDEX_SMTP_MAIL_FROM'),
     MAIL_PASSWORD = os.environ.get('YANDEX_SMTP_PASSWORD'),
     MAIL_FROM = os.environ.get('YANDEX_SMTP_MAIL_FROM'),
     MAIL_PORT = os.environ.get('YANDEX_SMTP_MAIL_PORT', 465),
-    MAIL_SERVER = os.environ.get('YANDEX_SMTP_MAIL_SERVER'),
+    MAIL_SERVER = os.environ.get('YANDEX_SMTP_MAIL_SERVER', 'smtp.yandex.ru'),
     MAIL_DEBUG = int(os.environ.get('YANDEX_SMTP_DEBUG_MODE'), 0),
     MAIL_TLS = True,
     MAIL_SSL = False,
