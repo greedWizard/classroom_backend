1283c10ac6fe635abcc21d611a6e33237d97acdc
v.bogdanenok <v.bogdanenok@benovate.ru>
2022-05-25 15:11:15 +0300

    migration fixes and update/create timestamp fix

--- common/models.py
+++ common/models.py
@@ -5,8 +5,8 @@ from tortoise import (
 
 
 class TimeStampAbstract(models.Model):
-    created_at = fields.DatetimeField(auto_now=True)
-    updated_at = fields.DatetimeField(auto_now_add=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now=True)
 
     class Meta:
         abstract = True
