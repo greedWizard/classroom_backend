bc66f447bb0fb0438186bfb7df3bb7ef234cdc1e
Vyacheslav Bogdanyonok <skavik46111@gmail.com>
2022-12-27 00:43:24 +0300

    added certbot support (fix)

--- docker/start_with_ssl.sh
+++ docker/start_with_ssl.sh
@@ -1,1 +1,7 @@
-poetry run uvicorn app:app --host 0.0.0.0 --port 8000 --reload --ssl-keyfile /etc/letsencrypt/live/bezpart.ru/privkey.pem --ssl-certfile /etc/letsencrypt/live/bezpart.ru/fullchain.pem
+FILE=/etc/letsencrypt/live/bezpart.ru/privkey.pem
+
+if test -f "$FILE"; then
+    poetry run uvicorn app:app --host 0.0.0.0 --port 8000 --reload --ssl-keyfile /etc/letsencrypt/live/bezpart.ru/privkey.pem --ssl-certfile /etc/letsencrypt/live/bezpart.ru/fullchain.pem
+else
+    poetry run uvicorn app:app --host 0.0.0.0 --port 8000 --reload
+fi
