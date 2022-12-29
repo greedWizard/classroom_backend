f390749c836342f8c7d25fb5b7b26fd046df03b8
Vyacheslav Bogdanyonok <skavik46111@gmail.com>
2022-12-29 12:31:32 +0300

    nginx stayed for backend

--- docker/start_with_ssl.sh
+++
@@ -1,7 +0,0 @@
-FILE=/etc/letsencrypt/live/bezpart.ru/privkey.pem
-
-if test -f "$FILE"; then
-    poetry run uvicorn app:app --host 0.0.0.0 --port 8000 --reload --ssl-keyfile /etc/letsencrypt/live/bezpart.ru/privkey.pem --ssl-certfile /etc/letsencrypt/live/bezpart.ru/fullchain.pem
-else
-    poetry run uvicorn app:app --host 0.0.0.0 --port 8000 --reload
-fi
