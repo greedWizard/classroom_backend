91b245267b27de0d87b76b0135af3d80ace5e9df
Vyacheslav Bogdanyonok <skavik46111@gmail.com>
2023-01-12 21:31:49 +0300

    added dockerc-compose prod file

--- docker/start_prod.sh
+++ docker/start_prod.sh
@@ -1,1 +1,1 @@
-uvicorn app:app --ssl-keyfile /etc/letsencrypt/live/bezpart.ru/privkey.pem --ssl-keyfile /etc/letsencrypt/live/bezpart.ru/fullchain.pem
+poetry run uvicorn app:app --ssl-keyfile /etc/letsencrypt/live/bezpart.ru/privkey.pem --ssl-keyfile /etc/letsencrypt/live/bezpart.ru/fullchain.pem
